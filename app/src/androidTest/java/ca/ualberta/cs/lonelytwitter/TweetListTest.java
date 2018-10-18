24e7b27c4070acd9ef7820b787754b826a8e84e9
PeterMa9621 <jma4@ualberta.ca>
2018-10-17 21:49:02 -0600

    Update

---
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -0,0 +1,32 @@
+import android.test.ActivityInstrumentationTestCase2;
+
+public class TweetListTest extends ActivityInstrumentationTestCase2
+{
+    public TweetListTest()
+    {
+        super(TweetList.class);
+    }
+
+    public void AddTweetTest()
+    {
+        Tweet tweet = new Tweet("Tweet");
+        TweetList.addTweet(tweet);
+
+        assertFalse(TweetList.getTweet(0)==tweet);
+    }
+
+    public void GetCountTest()
+    {
+        Tweet tweet = new Tweet("Tweet");
+        TweetList.addTweet(tweet);
+        assertFalse(TweetList.getCount()==1);
+    }
+
+    public void HasTweetTest()
+    {
+        Tweet tweet = new Tweet("Tweet");
+        TweetList.addTweet(tweet);
+        assertFalse(TweetList.hasTweet(tweet)==true);
+    }
+    
+}
